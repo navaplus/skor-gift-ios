2c4071a7e8963bd3527be3e34f8d9aa4b555d359
Lynda Erwan <lynda@dihardjasoftware.com>
2018-05-03 14:28:33 +0700

    add to home page table header view and table view.

--- Skor Gift/Classes/ViewController/Home/HomeViewController.swift
+++ Skor Gift/Classes/ViewController/Home/HomeViewController.swift
@@ -8,9 +8,35 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var tableHeaderView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: "VoucherListTableViewCell", bundle: nil), forCellReuseIdentifier: "VoucherListTableViewCell")
+        
+        tableView.tableHeaderView = tableHeaderView
+    }
+    
+    //MARK: - UITableViewDataSource, UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VoucherListTableViewCell", for: indexPath) as! VoucherListTableViewCell
+        cell.selectionStyle = .default
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
