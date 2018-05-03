ff544dd57c38567d1375321ba751d7161f4a1da3
Lynda Erwan <lynda@dihardjasoftware.com>
2018-05-03 12:45:18 +0700

    create gift and discount page's table view cell and it's ui component, add tableview and table view cell in gift and discount vc.

---
+++ Skor Gift/Classes/CustomCell/GiftDiscountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GiftDiscountTableViewCell.swift
+//  Skor Gift
+//
+//  Created by Lynda Erwan on 03/05/18.
+//  Copyright © 2018 Skor. All rights reserved.
+//
+
+import UIKit
+
+class GiftDiscountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Skor Gift/Classes/ViewController/Discount/DiscountsViewController.swift
+++ Skor Gift/Classes/ViewController/Discount/DiscountsViewController.swift
@@ -8,9 +8,41 @@
 
 import UIKit
 
-class DiscountsViewController: UIViewController {
+class DiscountsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: "GiftDiscountTableViewCell", bundle: nil), forCellReuseIdentifier: "GiftDiscountTableViewCell")
+    }
+    
+    //MARK: - UITableViewDataSource, UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GiftDiscountTableViewCell", for: indexPath) as! GiftDiscountTableViewCell
+        cell.selectionStyle = .default
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else if indexPath.row == 1 {
+            
+        } else if indexPath.row == 2 {
+            
+        } else if indexPath.row == 3 {
+            
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
